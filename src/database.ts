f0556660bd0db4c39e0ae5ab326e1f78e33a5c6a
Charlie Gutierrez <36178883+famousuni@users.noreply.github.com>
2022-01-01 09:29:07 -0500

    Add database client
    Added database.ts which containers logic for creating the database client object

---
+++ src/database.ts
@@ -0,0 +1,29 @@
+import dotenv from 'dotenv'
+import { Pool } from 'pg'
+
+dotenv.config()
+
+const { PG_USER, PG_PASSWORD, PG_DB, PG_TEST_DB, PG_HOST, ENV } = process.env
+
+console.log(`Database Environment is : ${ENV}`)
+let Client: Pool
+
+if (ENV === 'test') {
+    Client = new Pool({
+        host: PG_HOST,
+        user: PG_USER,
+        password: PG_PASSWORD,
+        database: PG_TEST_DB
+    })
+}
+
+if (ENV === 'dev') {
+    Client = new Pool({
+        host: PG_HOST,
+        user: PG_USER,
+        password: PG_PASSWORD,
+        database: PG_DB
+    })
+}
+
+export { Client }
