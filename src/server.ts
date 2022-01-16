f2fe23bb4f837869817db9a3b97ee1d9f5bfc1fa
Charlie Gutierrez <36178883+famousuni@users.noreply.github.com>
2022-01-16 14:12:34 -0500

    Fix DevDB Issue

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,11 @@ const app: express.Application = express()
 const address = '0.0.0.0:3000'
 
 app.use(bodyParser.json())
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/', function (req: Request, res: Response) {
   res.send('Hello World!')
