9206c14a07629d10a8b42655ef9606698a2c2b3d
Charlie Gutierrez <36178883+famousuni@users.noreply.github.com>
2022-01-17 12:59:49 -0500

    Add auth header to cors accept

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ const address = '0.0.0.0:3000'
 app.use(bodyParser.json())
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization");
   next();
 });
 
